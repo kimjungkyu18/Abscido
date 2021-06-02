7b6946f1cfadbc8930adef5952fa10fec1510f62
Jungkyu Kim <jungkyuunt@gmail.com>
2021-06-02 16:42:26 -0400

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -15,27 +15,83 @@ class App extends Component {
   }
 
   async loadBlockchainData(dispatch) {
-
     //check if MetaMask exists
-
+    if (typeof window.ethereum !== 'undefined'){
       //assign to values to variables: web3, netId, accounts
+      const web3 = new Web3(window.ethereum)
+      const netId = await web3.eth.net.getId()
+      console.log(netId)
 
-      //check if account is detected, then load balance&setStates, elsepush alert
+      // Ask client to connect MetaMask to app, allowing us to access accounts
+      await window.ethereum.enable() //(implement eth_requestAccounts at a later point)
+      const accounts = await web3.eth.getAccounts()
+      console.log(accounts)
 
-      //in try block load contracts
+      //check if account is detected, then load balance&setStates, elsepush alert 
+      if (typeof accounts[0] !== 'undefined') {
+        const balance = await web3.eth.getBalance(accounts[0])
+        console.log(balance)
+        this.setState({ account: accounts[0], balance: balance, web3: web3})
+      } else {
+        window.alert('Please connect to site using MetaMask')
+      }
+
+      // Create contract objects to interact with contracts on the blockchain
+      try {
+        const token = new web3.eth.Contract(Token.abi, Token.networks[netId].address)
+        const dbank = new web3.eth.Contract(dBank.abi, dBank.networks[netId].address)
+        const dBankAddress = dBank.networks[netId].address
+        this.setState({ token: token, dbank: dbank})
+        console.log(token.options.address)
+      } catch(e) {
+        console.log('Error', e)
+        window.alert('Contracts not deployed to the current network')
+      }
+
 
     //if MetaMask not exists push alert
+    } else {
+      window.alert('Please install MetaMask')
+    }
   }
 
   async deposit(amount) {
-    //check if this.state.dbank is ok
-      //in try block call dBank deposit();
+    if (this.state.dbank !== 'undefined') {
+      try {
+        await this.state.dbank.methods.deposit().send({value: amount.toString(), from: this.state.account})
+      } catch (e) {
+        console.log('Error, deposit: ', e)
+      }
+    }
   }
 
   async withdraw(e) {
     //prevent button from default click
     //check if this.state.dbank is ok
-    //in try block call dBank withdraw();
+    if (this.state.dbank !== 'undefined') {
+      try {
+        await this.state.dbank.methods.withdraw().send({from: this.state.account})
+      } catch (e) {
+        console.log('Error, withdraw: ', e)
+      }
+    }
+  }
+
+  async calculateInterest() {
+    const receipt = await this.state.dbank.methods.checkTimePassed().call({from: this.state.account})
+    console.log(receipt)
+    const hodlTime = Date.now() / 1000 - receipt[0]
+    const userBalance = receipt[1]
+    const interestPerSecond = 31668017 * (userBalance / 1e16);
+    const interest = interestPerSecond * hodlTime
+    this.setState({interest: interest})
+  }
+
+  async checkInterest() {
+    const receipt = await this.state.dbank.methods.checkInterest().send({from: this.state.account})
+    let interest = receipt.events.CheckInterest.returnValues[2]
+    this.setState({interest: interest})
+    console.log('interest:', this.state.interest)
   }
 
   constructor(props) {
@@ -46,6 +102,7 @@ class App extends Component {
       token: null,
       dbank: null,
       balance: 0,
+      interest: 0, // In ETH
       dBankAddress: null
     }
   }
@@ -56,25 +113,71 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href="DBC"
             target="_blank"
             rel="noopener noreferrer"
           >
         <img src={dbank} className="App-logo" alt="logo" height="32"/>
-          <b>dBank</b>
+          <b>Abscido</b>
         </a>
         </nav>
         <div className="container-fluid mt-5 text-center">
         <br></br>
-          <h1>{/*add welcome msg*/}</h1>
-          <h2>{/*add user address*/}</h2>
+          <h1>Welcome to Abscido</h1>
+          <h2>Account: {this.state.account}</h2>
           <br></br>
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-              <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-                {/*add Tab deposit*/}
-                {/*add Tab withdraw*/}
+              <Tabs defaultActiveKey="check-interest" id="uncontrolled-tab-example">
+                <Tab eventKey="deposit" title="Deposit">
+                  <div>
+                    How much do you want to deposit?
+                    <br></br>
+                    Minimum is 0.01 ETH.
+                    <br></br>
+                    Only 1 deposit is allowed at a time
+                    <form onSubmit={(e) => {
+                      e.preventDefault() // Prevents refreshing of page after click
+                      let amount = this.depositAmount.value // Grab deposit amount through ref
+                      this.setState({ deposit: amount}) // Set deposit state to amount in ETH
+                      amount = amount * 10 ** 18 // Convert from ETH to wei
+                      this.deposit(amount)
+                      }}>
+                    <div className='form-group mr-sm-2'>
+                      <br></br>
+                      <input
+                        id='depositAmount'
+                        step='0.01'
+                        type='number'
+                        ref={(input) => { this.depositAmount = input }}
+                        className='form-control form-control-md'
+                        placeholder='Amount...'
+                        required
+                        min='0.01'
+                        />
+                    </div>
+                    <button type='submit' className='btn btn-primary'>DEPOSIT</button>
+                    </form>
+                  </div>
+                </Tab>
+                <Tab eventKey="withdraw" title="Withdraw">
+                  <div>Do you wish to withdraw?</div>
+                  <br></br>
+                  <button type='submit' className='btn btn-primary' onClick={(e) => {
+                    e.preventDefault()
+                    this.withdraw()
+                  }}>WITHDRAW</button>
+                </Tab>
+                <Tab eventKey="check-interest" title="Check Interest">
+                <div>Check interest here!</div>
+                  <br></br>
+                  <button type='submit' className='btn btn-primary' onClick={(e) => {
+                    e.preventDefault()
+                    this.calculateInterest()
+                  }}>CHECK INTEREST</button>
+                  <div>Interest: {this.state.interest}</div>
+                </Tab>
               </Tabs>
               </div>
             </main>
--- src/contracts/dBank.sol
+++ src/contracts/dBank.sol
@@ -4,71 +4,145 @@ pragma solidity >=0.6.0 <0.8.0;
 import "./Token.sol";
 
 contract dBank {
+    //assign Token contract to variable
+    Token private token;
 
-  //assign Token contract to variable
+    //add mappings
+    mapping(address => uint256) public etherBalanceOf;
+    mapping(address => uint256) public depositStart;
+    mapping(address => bool) public isDeposited;
 
-  //add mappings
+    //add events
+    event Deposit(
+        address indexed _from,
+        uint256 _etherAmount,
+        uint256 _timeStart
+    );
 
-  //add events
+    event Withdraw(
+        address indexed _from,
+        uint256 _etherAmount,
+        uint256 _interestEared,
+        uint256 _withdrawTime
+    );
 
-  //pass as constructor argument deployed Token contract
-  constructor() public {
-    //assign token deployed contract to variable
-  }
+    event CheckInterest(
+        address indexed _from,
+        uint256 _etherAmount,
+        uint256 _interestEared
+    );
 
-  function deposit() payable public {
-    //check if msg.sender didn't already deposited funds
-    //check if msg.value is >= than 0.01 ETH
+    event CheckTimePassed(address indexed _from, uint256 _timePassed);
 
-    //increase msg.sender ether deposit balance
-    //start msg.sender hodling time
+    //pass as constructor argument deployed Token contract
+    constructor(Token _token) public {
+        //assign token deployed contract to variable
+        token = _token;
+    }
 
-    //set msg.sender deposit status to true
-    //emit Deposit event
-  }
+    function deposit() public payable {
+        //check if msg.sender didn't already deposited funds
+        require(!isDeposited[msg.sender], "Error: Deposit already active");
 
-  function withdraw() public {
-    //check if msg.sender deposit status is true
-    //assign msg.sender ether deposit balance to variable for event
+        //check if msg.value is >= than 0.01 ETH
+        require(
+            msg.value >= 0.01 ether,
+            "Error: Deposit must be greater than 0.01 ETH"
+        );
 
-    //check user's hodl time
+        //increase msg.sender ether deposit balance
+        etherBalanceOf[msg.sender] += msg.value;
 
-    //calc interest per second
-    //calc accrued interest
+        //start msg.sender hodling time
+        depositStart[msg.sender] += block.timestamp;
 
-    //send eth to user
-    //send interest in tokens to user
+        //set msg.sender deposit status to true
+        isDeposited[msg.sender] = true;
 
-    //reset depositer data
+        //emit Deposit event
+        emit Deposit(msg.sender, msg.value, block.timestamp);
+    }
 
-    //emit event
-  }
+    function withdraw() public {
+        //check if msg.sender deposit status is true
+        require(
+            isDeposited[msg.sender],
+            "Error: You have not yet deposited ETH"
+        );
+        //assign msg.sender ether deposit balance to variable for event
+        uint256 userBalance = etherBalanceOf[msg.sender];
 
-  function borrow() payable public {
-    //check if collateral is >= than 0.01 ETH
-    //check if user doesn't have active loan
+        //check user's hodl time
+        uint256 hodlTime = block.timestamp - depositStart[msg.sender];
 
-    //add msg.value to ether collateral
+        //calc accrued interest assuming 10% interest in wei
+        uint256 interestPerSecond =
+            31668017 * (etherBalanceOf[msg.sender] / 1e16);
+        uint256 interest = interestPerSecond * hodlTime;
 
-    //calc tokens amount to mint, 50% of msg.value
+        //send eth to user
+        msg.sender.transfer(userBalance);
 
-    //mint&send tokens to user
+        //send interest in tokens to user
+        token.mint(msg.sender, interest);
 
-    //activate borrower's loan status
+        //reset depositer data
+        etherBalanceOf[msg.sender] = 0;
+        depositStart[msg.sender] = 0;
+        isDeposited[msg.sender] = false;
 
-    //emit event
-  }
+        //emit event, convert interestEarned from wei to DBC assuming DBC has same wei conversion rates as ETH
+        emit Withdraw(
+            msg.sender,
+            userBalance,
+            interest / 1e18,
+            block.timestamp
+        );
+    }
 
-  function payOff() public {
-    //check if loan is active
-    //transfer tokens from user back to the contract
+    function checkInterest() public {
+        require(isDeposited[msg.sender]);
+        //assign msg.sender ether deposit balance to variable for event
+        uint256 userBalance = etherBalanceOf[msg.sender];
 
-    //calc fee
+        //check user's hodl time
+        uint256 hodlTime = block.timestamp - depositStart[msg.sender];
 
-    //send user's collateral minus fee
+        //calc accrued interest assuming 10% interest in wei
+        uint256 interestPerSecond = 31668017 * (userBalance / 1e16);
+        uint256 interest = interestPerSecond * hodlTime;
 
-    //reset borrower's data
+        emit CheckInterest(msg.sender, userBalance, interest);
+    }
 
-    //emit event
-  }
+    function checkTimePassed()
+        public
+        view
+        returns (uint256 _depositStart, uint256 _amountDeposited)
+    {
+        require(
+            isDeposited[msg.sender],
+            "Error: You have not deposited ETH yet"
+        );
+        return (depositStart[msg.sender], etherBalanceOf[msg.sender]);
+    }
+
+    function borrow() public payable {
+        //check if collateral is >= than 0.01 ETH
+        //check if user doesn't have active loan
+        //add msg.value to ether collateral
+        //calc tokens amount to mint, 50% of msg.value
+        //mint&send tokens to user
+        //activate borrower's loan status
+        //emit event
+    }
+
+    function payOff() public {
+        //check if loan is active
+        //transfer tokens from user back to the contract
+        //calc fee
+        //send user's collateral minus fee
+        //reset borrower's data
+        //emit event
+    }
 }
